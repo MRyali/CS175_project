8bf8ad5bee75dd9997b5f604e2f27ddc936fd37e
Alex Kevakian <akeva001@ucr.edu>
2021-06-09 22:53:23 -0700

    sign up page

--- cs175_mvp/lib/main.dart
+++ cs175_mvp/lib/main.dart
@@ -11,47 +11,101 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //final callKey = Globalkey<PageViewDemo>();
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading: IconButton(icon: Icon(Icons.arrow_back_ios)),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Image.network(
-              'https://bayareacrisisnursery.org/wp-content/uploads/2019/08/Volunteer.jpg'),
-          new Container(
-            padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-            child: new Text(
-              'Make Blessing Bags',
-              style: new TextStyle(
-                fontSize: 30.0,
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
+      body: Center(
+        child: Scrollbar(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(50),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      // Text(
+                      //   'Welcome!',
+                      //   style: TextStyle(
+                      //       color: Colors.green,
+                      //       fontSize: 50.0,
+                      //       fontWeight: FontWeight.bold),
+                      // ),
+                    ],
+                  ),
+                ),
+                TextFormField(
+                  autofocus: true,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    filled: true,
+                    labelText: 'First Name',
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(3.0),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    filled: true,
+                    labelText: 'Last Name',
+                  ),
+                  obscureText: true,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(3.0),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    filled: true,
+                    labelText: 'Phone #',
+                  ),
+                  obscureText: true,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(3.0),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    filled: true,
+                    labelText: 'Email Address',
+                  ),
+                  obscureText: true,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                ),
+                RaisedButton(
+                  child: Text(
+                    '                 Sign up                   ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25.0,
+                    ),
+                  ),
+                  color: Colors.green,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(22.0))),
+                  padding: EdgeInsets.all(10),
+                  onPressed: () {},
+                ),
+              ],
             ),
           ),
-          new Container(
-            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-            child: new Text(
-              'Create care packages of snacks and bottled water to hand out to homeless people you may encounter while driving.',
-              style: new TextStyle(
-                fontSize: 20.0,
-                color: Colors.black,
-              ),
-            ),
-          ),
-          new Container(
-            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-            alignment: AlignmentDirectional.center,
-            child: ElevatedButton.icon(
-              icon: Text('Sign up'),
-              label: Icon(Icons.arrow_forward, size: 16),
-              onPressed: () => {},
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
