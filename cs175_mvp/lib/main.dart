330851e2e72572f7f0c0d0c31b16e7dd147e62b9
Alex Kevakian <akeva001@ucr.edu>
2021-06-09 21:04:22 -0700

    Opportunity Details Page

--- cs175_mvp/lib/main.dart
+++ cs175_mvp/lib/main.dart
@@ -13,28 +13,45 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          'Volunteer Opportunities',
-          style: TextStyle(height: 0, fontSize: 30),
-        ),
+        backgroundColor: Colors.white,
+        leading: IconButton(icon: Icon(Icons.arrow_back_ios)),
       ),
-      body: Center(
-        child: Scrollbar(
-          child: ListView.builder(
-              itemCount: 50,
-              itemBuilder: (context, index) {
-                return Card(
-                    color: Colors.grey[400],
-                    child: ListTile(
-                      //backgroundColor: Colors.blue,
-                      title: Text(
-                          'Posting # ${index + 1}',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ));
-              }),
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Image.network(
+              'https://bayareacrisisnursery.org/wp-content/uploads/2019/08/Volunteer.jpg'),
+          new Container(
+            padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+            child: new Text(
+              'Make Blessing Bags',
+              style: new TextStyle(
+                fontSize: 30.0,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          new Container(
+            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+            child: new Text(
+              'Create care packages of snacks and bottled water to hand out to homeless people you may encounter while driving.',
+              style: new TextStyle(
+                fontSize: 20.0,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          new Container(
+            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+            alignment: AlignmentDirectional.center,
+            child: ElevatedButton.icon(
+              icon: Text('Sign up'),
+              label: Icon(Icons.arrow_forward, size: 16),
+              onPressed: () => {},
+            ),
+          ),
+        ],
       ),
     );
   }
