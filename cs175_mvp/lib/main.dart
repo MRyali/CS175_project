33ae80bb2e4b0090f92837702b539b83079cb876
MRyali <mayurryali29@gmail.com>
2021-06-09 22:12:31 -0700

    Finshing Touches

--- cs175_mvp/lib/main.dart
+++ cs175_mvp/lib/main.dart
@@ -7,8 +7,7 @@ void main() {
   ));
 }
 
-class ScrollBar extends StatelessWidget {
-  // This widget is the root of your application.
+class Postings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,15 +47,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Generous',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.green,
       ),
       home: Scaffold(
@@ -154,8 +145,7 @@ class _LoginPageState extends State<LoginPage> {
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => ScrollBar()),
-
+                      MaterialPageRoute(builder: (context) => Postings()),
                     );
                   },
                 ),
@@ -207,7 +197,7 @@ class _PageViewDemoState extends State<PageViewDemo> {
       controller: _controller,
       children: [
         MyApp(),
-        ScrollBar(),
+        //Postings(),
         PopUp(),
       ],
     );
@@ -215,7 +205,6 @@ class _PageViewDemoState extends State<PageViewDemo> {
 }
 
 class PopUp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
