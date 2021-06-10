ad8655e6edbd502ad232b5944fffdefa4009c436
MRyali <mayurryali29@gmail.com>
2021-06-09 23:11:10 -0700

    Add signup

--- cs175_mvp/lib/main.dart
+++ cs175_mvp/lib/main.dart
@@ -245,7 +245,10 @@ class PopUp extends StatelessWidget {
               icon: Text('Sign up'),
               label: Icon(Icons.arrow_forward, size: 16),
               onPressed: () => {
-
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignUp()),
+                ),
               },
               style: ElevatedButton.styleFrom(
                   primary: Colors.green,
@@ -253,6 +256,138 @@ class PopUp extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class EndPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var title = 'Thank You';
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center (
+            child: Text('You have successfully signed up!',
+                style: TextStyle(fontSize: 25),
+            ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignUp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    //final callKey = Globalkey<PageViewDemo>();
+    return Scaffold(
+      body: Center(
+        child: Scrollbar(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(50),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      // Text(
+                      //   'Welcome!',
+                      //   style: TextStyle(
+                      //       color: Colors.green,
+                      //       fontSize: 50.0,
+                      //       fontWeight: FontWeight.bold),
+                      // ),
+                    ],
+                  ),
+                ),
+                TextFormField(
+                  autofocus: true,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    filled: true,
+                    labelText: 'First Name',
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(3.0),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    filled: true,
+                    labelText: 'Last Name',
+                  ),
+                  obscureText: true,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(3.0),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    filled: true,
+                    labelText: 'Phone #',
+                  ),
+                  obscureText: true,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(3.0),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    filled: true,
+                    labelText: 'Email Address',
+                  ),
+                  obscureText: true,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                ),
+                RaisedButton(
+                  child: Text(
+                    '                 Sign up                   ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25.0,
+                    ),
+                  ),
+                  color: Colors.green,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(22.0))),
+                  padding: EdgeInsets.all(10),
+                  onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => EndPage()),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
