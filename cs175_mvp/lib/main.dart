cf9ea758f1c3f3be23e0ef30db40921400e0ff69
MRyali <mayurryali29@gmail.com>
2021-06-09 19:49:38 -0700

    Link Pages

--- cs175_mvp/lib/main.dart
+++ cs175_mvp/lib/main.dart
@@ -1,23 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 
 void main() {
   runApp(MaterialApp(
+    title: 'Navigation Basics',
     home: MyApp(),
   ));
 }
 
-class MyApp extends StatelessWidget {
+/*
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Open route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //Navigator.pop(context);
+            Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ScrollBar()),
+            );
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+*/
+class ScrollBar extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
+        title: Text("Volunteer Opportunities"),
+        //backgroundColor: Colors.black,
+        /*title: Text(
           'Volunteer Opportunities',
           style: TextStyle(height: 0, fontSize: 30),
-        ),
+        ),*/
       ),
       body: Center(
         child: Scrollbar(
@@ -29,11 +76,147 @@ class MyApp extends StatelessWidget {
                     child: ListTile(
                       //backgroundColor: Colors.blue,
                       title: Text(
-                          'Posting # ${index + 1}',
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                        'Posting # ${index + 1}',
+                        style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                     ));
               }),
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Generous',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.green,
+      ),
+      home: LoginPage(title: 'Login'),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextStyle loginStyle = TextStyle(
+      fontSize: 25.0, fontWeight: FontWeight.w300, fontFamily: 'Open Sans');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Scrollbar(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(50),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: new Image.asset('assets/generous.png',
+                      width: 100.0, height: 100.0),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      // Text(
+                      //   'Welcome!',
+                      //   style: TextStyle(
+                      //       color: Colors.green,
+                      //       fontSize: 50.0,
+                      //       fontWeight: FontWeight.bold),
+                      // ),
+                    ],
+                  ),
+                ),
+                TextFormField(
+                  autofocus: true,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    filled: true,
+                    labelText: 'Username',
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(3.0),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    filled: true,
+                    labelText: 'Password',
+                  ),
+                  obscureText: true,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                ),
+                RaisedButton(
+                  child: Text(
+                    '                   Login                   ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25.0,
+                    ),
+                  ),
+                  color: Colors.green,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(22.0))),
+                  padding: EdgeInsets.all(10),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ScrollBar()),
+                    );
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Forgot username/password?',
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                      Text(
+                        'Need an account? Sign up',
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
